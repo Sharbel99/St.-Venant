32744320d86319f755b1594d06862cd24a0aa374
Sharbel <sharbel.nashar@std.balamand.edu.lb>
2019-04-02 10:58:07 +0300

    Basilisk, tutorial 1 part 12

--- bump.c
+++ bump.c
@@ -1,7 +1,21 @@
+#include "grid/cartesian.h"
 #include "saint-venant.h"
-event end ( i=10) {
+event init (t=0) {
+ foreach()
+  h[] = 0.1 + 1.*exp(-200.*(x*x + y*y));
+}
+event graphs (i++){
+stats s = statsf (h);
+fprintf (stderr, "%g %g %g\n", t, s.min, s.max);
+}
+event images (t += 4./300.){
+output_ppm (h);
+}
+event end (t = 4) {
 printf ("i = %d t = %g\n", i, t);
 }
 int main() {
+origin(-0.5, -0.5);
+init_grid (256);
 run();
 }
